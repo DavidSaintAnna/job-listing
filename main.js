63c6806b38b0dbac4d1ebfd5b065860cc2625d48
David <david.henri.santana@gmail.com>
2023-12-26 20:54:19 -0300

    refactor: innerhtml bad practice

--- main.js
+++ main.js
@@ -1,10 +1,62 @@
 const $cards = document.querySelector(".cards");
+const $modal = document.querySelector(".modal");
+
+const triggerModal = (event) => {
+  $modal.setAttribute("style", "display:block");
+};
 
 fetch("./data.json")
   .then((response) => response.json())
   .then((jsonData) => {
     jsonData.forEach((item) => {
       const cardDiv = document.createElement("div");
+
+      const firstRow = document.createElement("div");
+      const secondRow = document.createElement("div");
+      const thirdRow = document.createElement("div");
+      const grayLine = document.createElement("div");
+      const gridRow = document.createElement("div");
+      firstRow.classList.add("first-row");
+      secondRow.classList.add("second-row");
+      thirdRow.classList.add("third-row");
+      grayLine.classList.add("bar", "hide");
+      gridRow.classList.add("grid-row");
+
+      const company = document.createElement("span");
+      company.classList.add("blue-span");
+      company.innerText = item.company;
+      firstRow.appendChild(company);
+
+      if (item.new) {
+        const newSpan = document.createElement("span");
+        newSpan.innerText = "NEW!";
+        firstRow.appendChild(newSpan);
+      }
+
+      if (item.featured) {
+        const newSpan = document.createElement("span");
+        newSpan.innerText = "FEATURED";
+        firstRow.appendChild(newSpan);
+      }
+
+      const contractData = (innerText) => {
+        const tag = document.createElement("span");
+        tag.innerText = innerText;
+        thirdRow.appendChild(tag);
+      };
+
+      contractData(item.postedAt);
+      thirdRow.appendChild(document.createTextNode("\u2022"));
+      contractData(item.contract);
+      thirdRow.appendChild(document.createTextNode("\u2022"));
+      contractData(item.location);
+
+      const createTag = (innerText) => {
+        const tag = document.createElement("span");
+        tag.innerText = innerText;
+        tag.addEventListener("click", triggerModal);
+        gridRow.appendChild(tag);
+      };
 
       if (item.new && item.featured) {
         cardDiv.classList.add("card");
@@ -14,48 +66,33 @@ fetch("./data.json")
         cardDiv.classList.add("card-not-featured");
       }
 
-      const positionArray = item.position.split(" ");
-      let positionSpans = "";
-      positionArray.forEach((word) => {
-        positionSpans += `<span><strong>${word}</strong></span> `;
-      });
-      let gridRowContent = `<span>${item.role}</span><span>${item.level}</span>`;
+      const strong = document.createElement("strong");
+      strong.innerText = item.position;
+      secondRow.appendChild(strong);
+
+      createTag(item.role);
+      createTag(item.level);
+
       for (
         let i = 0;
         i < Math.max(item.languages.length, item.tools.length);
         i++
       ) {
         if (item.languages[i]) {
-          gridRowContent += `<span>${item.languages[i]}</span>`;
+          cardDiv.classList.add(`language-${item.languages[i]}`);
+          createTag(item.languages[i]);
         }
         if (item.tools[i]) {
-          gridRowContent += `<span>${item.tools[i]}</span>`;
+          cardDiv.classList.add(`tool-${item.tools[i]}`);
+          createTag(item.tools[i]);
         }
       }
 
-      const htmlContent = `
-      <div class="first-row">
-            <span class="blue-span">${item.company}</span>
-           ${item.new ? "<span>NEW!</span>" : ""}
-      ${item.featured ? "<span>FEATURED</span>" : ""}
-          </div>
-          <div class="second-row">
-           ${positionSpans}
-          </div>
-          <div class="third-row">
-            <span>${item.postedAt}</span>
-            &#8226;
-            <span>${item.contract}</span>
-            &#8226;
-            <span>${item.location}</span>
-          </div>
-          <div class="bar hide"></div>
-          <div class="grid-row">
-           ${gridRowContent}
-        </div>
-      `;
-
-      cardDiv.innerHTML = htmlContent;
+      cardDiv.appendChild(firstRow);
+      cardDiv.appendChild(secondRow);
+      cardDiv.appendChild(thirdRow);
+      cardDiv.appendChild(grayLine);
+      cardDiv.appendChild(gridRow);
 
       $cards.appendChild(cardDiv);
     });
